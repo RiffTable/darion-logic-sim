58f93ec570dfb6e3d9738810e43d564d1e6042ae
Riff <jarif.m.aabir@gmail.com>
2026-03-01 17:31:10 +0600

    Cheeky trick to fix pyright autocorrect

--- core/LogicCore.py
+++ core/LogicCore.py
@@ -1,10 +1,23 @@
 import sys
 import os
+from typing import TYPE_CHECKING
 sys.path.append(os.path.join(os.getcwd(), 'engine'))
-import Const
-from Circuit import Circuit
-from Gates import Gate, AND, NAND, OR, NOR, XOR, XNOR, NOT, Variable, Probe, InputPin, OutputPin
-from IC import IC
+
+# This is because pyright is having trouble importing without absolute paths
+# but I can't bc that causes other *weird* issue
+# This solution is beyond checky and will probably break >:3
+if TYPE_CHECKING:
+	from engine import Const
+	from engine.Circuit import Circuit
+	from engine.Gates import Gate, AND, NAND, OR, NOR, XOR, XNOR, NOT, Variable, Probe, InputPin, OutputPin
+	from engine.IC import IC
+else:
+	import Const
+	from Circuit import Circuit
+	from Gates import Gate, AND, NAND, OR, NOR, XOR, XNOR, NOT, Variable, Probe, InputPin, OutputPin
+	from IC import IC
+
+
 
 
 logic = Circuit()
